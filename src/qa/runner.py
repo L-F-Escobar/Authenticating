da42cfdca631e1aa82ccf7b6081e182040218e37
Luis F. Escobar-Driver <lfescoba@uci.edu>
2018-06-06 10:50:21 -0700

    update user script complete

--- src/qa/runner.py
+++ src/qa/runner.py
@@ -1,12 +1,14 @@
 import unittest, xmlrunner, os
 
 from test import testCreateUser
+from test import testUpdateUser
 
 # Initialize a test loader & test suite package.
 loader = unittest.TestLoader()
 suite  = unittest.TestSuite()
 
-suite.addTests(loader.suiteClass(testCreateUser.suite()))
+# suite.addTests(loader.suiteClass(testCreateUser.suite()))
+suite.addTests(loader.suiteClass(testUpdateUser.suite()))
 
 # Initialize an xml runner.
 testRunner=xmlrunner.XMLTestRunner(output='data/testReports', verbosity=2)
